2b07a0e90dfea8894221fd7fe0a63ee55bd68822
sbs87 <steve@stevenbsmith.net>
2013-06-13 16:09:08 -0400

    initial commit

---
+++ plot_qBiomarker.py
@@ -0,0 +1,11 @@
+import bmath
+import sys
+from pylab import *
+
+dates=[1,2,4]
+dCt_species1=[1,2,10]
+dCt_species2=[5,2,9]
+
+plot(dates,dCt_species1,'r--')
+plot(dates, dCt_species2,'bs')
+show()
